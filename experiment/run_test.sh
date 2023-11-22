5bdc6052f43df6d2acba08d05d19a0cf8ff6494b
Erika Hunhoff <hunhoff.erika@gmail.com>
2023-11-21 19:39:37 -0700

    tput working without tna, fails with tna

--- experiment/run_test.sh
+++ experiment/run_test.sh
@@ -2,17 +2,25 @@
 
 INTRANODE_MODE="intranode"
 INTERNODE_MODE="internode"
-USAGE_STR="<$INTRANODE_MODE|$INTERNODE_MODE> <num_pairs> <outdir>"
-NUM_ARGS=3
+TPUT_TEST="tput"
+LAT_TEST="lat"
+USAGE_STR="<$INTRANODE_MODE|$INTERNODE_MODE> <$TPUT_TEST|$LAT_TEST> <num_pairs> <outdir>"
+NUM_ARGS=4
 SCRIPT_NAME=0
 ARG_NUM_MODE=1
-ARG_NUM_PAIRS=2
-ARG_NUM_OUTDIR=3
+ARG_NUM_TEST=2
+ARG_NUM_PAIRS=3
+ARG_NUM_OUTDIR=4
 
 MYDIR="$(dirname "$(realpath "$0")")"
 SERVERSPEC_FILE="$MYDIR/serverspec.yml"
 CLIENTSPEC_FILE="$MYDIR/clientspec.yml"
 TNA_NAMESPACE="tna-test"
+
+# TODO: for both command, how to config test duration?
+LAT_CMD="/opt/pkb/netperf-netperf-2.7.0/src/netperf -p 20000 -j -v2 -t TCP_RR -H REPLACE_ME_WITH_SERVER_IP -l 60 -- -P ,20001 -o THROUGHPUT,THROUGHPUT_UNITS,P50_LATENCY,P90_LATENCY,P99_LATENCY,STDDEV_LATENCY,MIN_LATENCY,MAX_LATENCY,CONFIDENCE_ITERATION,THROUGHPUT_CONFID,LOCAL_TRANSPORT_RETRANS,REMOTE_TRANSPORT_RETRANS,TRANSPORT_MSS"
+# TODO: is -M and -m okay? Should be parsed from machine config, I think?
+TPUT_CMD="/opt/pkb/netperf-netperf-2.7.0/src/netperf -p 20000 -j  -t TCP_STREAM -H REPLACE_ME_WITH_SERVER_IP -l 60  -- -P ,20001 -o THROUGHPUT,THROUGHPUT_UNITS,P50_LATENCY,P90_LATENCY,P99_LATENCY,STDDEV_LATENCY,MIN_LATENCY,MAX_LATENCY,CONFIDENCE_ITERATION,THROUGHPUT_CONFID,LOCAL_TRANSPORT_RETRANS,REMOTE_TRANSPORT_RETRANS,TRANSPORT_MSS -m 131072 -M 131072"
 
 ################# Argument parsing #######################
 
@@ -24,6 +32,7 @@ if [ $# != $NUM_ARGS ]; then
 fi
 
 mode=${!ARG_NUM_MODE}
+netperf_test=${!ARG_NUM_TEST}
 npairs=${!ARG_NUM_PAIRS}
 outdir=${!ARG_NUM_OUTDIR}
 
@@ -38,18 +47,28 @@ else
     exit -1
 fi
 
+# Check test is either throughput or latency
+if [ $netperf_test == $TPUT_TEST ] ; then
+    echo "==== Running throughput (TCP_STREAM) test"
+elif [ $netperf_test == $LAT_TEST ] ; then
+    echo "==== Running latency (TCP_RR) test"
+else
+    echo "***Error: Expected $TPUT_TEST or $LAT_TEST for test but is $netperf_test"
+    echo "$0: $USAGE_STR"
+    exit -1
+fi
+
 # Check number of pairs is valid
-if [ "${!ARG_NUM_PAIRS}" -eq "${!ARG_NUM_PAIRS}" ] 2>/dev/null
-then
+if [ "$npairs" -eq "$npairs" ] 2>/dev/null ; then
     :
 else
-    echo "ERROR: first parameter must be an integer."
+    echo "ERROR: Number of pairs must be an integer but is \"$npairs\"."
     echo $USAGE
     exit 1
 fi
 
-if [ ${!ARG_NUM_PAIRS} -lt 1 ] ; then
-    echo "***Error: Pairs to small, should be  >= 1 but is ${!ARG_NUM_PAIRS}"
+if [ $npairs -lt 1 ] ; then
+    echo "***Error: Pairs to small, should be  >= 1 but is $npairs"
     echo "$0: $USAGE_STR"
     exit -1
 else
@@ -82,8 +101,8 @@ else
     # use node 3
     clientNode=$(kubectl get no -o name | sed -n '3p' | cut -c6-)
 fi
-echo "==== Using node $serverNode for servers"
-echo "==== Using node $clientNode for clients"
+echo "==== Using node $serverNode for server(s)"
+echo "==== Using node $clientNode for client(s)"
 
 ############### Set up files ############################
 for ((i=1; i<=$npairs; i++)); do
@@ -101,6 +120,12 @@ for ((i=1; i<=$npairs; i++)); do
     sed -i "s/REPLACE_ME_WITH_NODE/$escapedClientNode/g" $clientFile
     sed -i "s/REPLACE_ME_WITH_CLIENT_NUM/$i/g" $clientFile
     sed -i "s/REPLACE_ME_WITH_NAMESPACE/$TNA_NAMESPACE/g" $clientFile
+    if [ $netperf_test == $TPUT_TEST ] ; then
+        sanitizedCmd=$(sed -e 's/[&\\/]/\\&/g; s/$/\\/' -e '$s/\\$//' <<<"$TPUT_CMD")
+    else
+        sanitizedCmd=$(sed -e 's/[&\\/]/\\&/g; s/$/\\/' -e '$s/\\$//' <<<"$LAT_CMD")
+    fi
+    sed -i "s/REPLACE_ME_WITH_CMD/$sanitizedCmd/g" $clientFile
     echo "==== Created client file: $clientFile"
 done
 
@@ -111,7 +136,7 @@ kubectl create namespace $TNA_NAMESPACE
 echo "Done!"
 
 ############### Start Server(s) #########################
-echo "==== Starting servers..."
+echo "==== Starting server(s)..."
 for ((i=1; i<=$npairs; i++)); do
     kubectl apply -f $outdir/server$i.yml
 done
@@ -127,11 +152,11 @@ do
     NUM_NOT_RUNNING=$((npairs-NUM_NOT_RUNNING))
 done
 echo ""
-echo "==== Server pods running! Waiting an extra 5 seconds before starting clients..."
+echo "==== Server pod(s) running! Waiting an extra 5 seconds before starting client(s)..."
 sleep 5
 
 ############### Start Clients ############################
-echo "==== Starting clients..."
+echo "==== Starting client(s)..."
 for ((i=1; i<=$npairs; i++)); do
     # Get server ip (we had to wait for the server to start running to get this)
     server_ip=$(kubectl get pod server-$i -n $TNA_NAMESPACE --template '{{.status.podIP}}')
