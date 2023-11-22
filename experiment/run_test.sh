9501276fa6ae4b7fd7b9364e9bf59f2d0f3fe930
Erika Hunhoff <hunhoff.erika@gmail.com>
2023-11-21 17:47:53 -0700

    test working for single pair

--- experiment/run_test.sh
+++ experiment/run_test.sh
@@ -2,11 +2,12 @@
 
 INTRANODE_MODE="intranode"
 INTERNODE_MODE="internode"
-USAGE_STR="<$INTRANODE_MODE|$INTERNODE_MODE> <num_pairs>"
-NUM_ARGS=2
+USAGE_STR="<$INTRANODE_MODE|$INTERNODE_MODE> <num_pairs> <outdir>"
+NUM_ARGS=3
 SCRIPT_NAME=0
 ARG_NUM_MODE=1
 ARG_NUM_PAIRS=2
+ARG_NUM_OUTDIR=3
 
 MYDIR="$(dirname "$(realpath "$0")")"
 SERVERSPEC_FILE="$MYDIR/serverspec.yml"
@@ -24,6 +25,7 @@ fi
 
 mode=${!ARG_NUM_MODE}
 npairs=${!ARG_NUM_PAIRS}
+outdir=${!ARG_NUM_OUTDIR}
 
 # Check mode is either internode or intranode
 if [ $mode == $INTRANODE_MODE ] ; then
@@ -54,6 +56,22 @@ else
     echo "==== Running $npairs pair(s) of pods"
 fi
 
+# Check that output dir does not exist
+if test -f $outdir; then
+    echo "***Error: Output directory $outdir already exists, and is a file."
+    echo "$0: $USAGE_STR"
+    exit -1
+fi
+if test -d $outdir; then
+    echo "***Error: Output directory $outdir already exists."
+    echo "$0: $USAGE_STR"
+    exit -1
+fi
+
+############### Create output directory ##################
+mkdir -p $outdir
+echo "==== Created output directory: $outdir"
+
 ############### Parse k8s nodes #########################
 
 # use node 2
@@ -68,29 +86,22 @@ echo "==== Using node $serverNode for servers"
 echo "==== Using node $clientNode for clients"
 
 ############### Set up files ############################
-ymlDir=$(mktemp -d)
-echo "==== Using temporary directory $ymlDir"
-
 for ((i=1; i<=$npairs; i++)); do
-    serverFile=$ymlDir/server$i.yml
+    serverFile=$outdir/server$i.yml
     cp $SERVERSPEC_FILE $serverFile
     escapedServerNode=$(sed -e 's/[&\\/]/\\&/g; s/$/\\/' -e '$s/\\$//' <<<"$serverNode")
     sed -i "s/REPLACE_ME_WITH_NODE/$escapedServerNode/g" $serverFile
     sed -i "s/REPLACE_ME_WITH_SERVER_NUM/$i/g" $serverFile
     sed -i "s/REPLACE_ME_WITH_NAMESPACE/$TNA_NAMESPACE/g" $serverFile
     echo "==== Created server file: $serverFile"
-    cat $serverFile
-    echo ""
 
-    clientFile=$ymlDir/client$i.yml
+    clientFile=$outdir/client$i.yml
     cp $CLIENTSPEC_FILE $clientFile
     escapedClientNode=$(sed -e 's/[&\\/]/\\&/g; s/$/\\/' -e '$s/\\$//' <<<"$clientNode")
     sed -i "s/REPLACE_ME_WITH_NODE/$escapedClientNode/g" $clientFile
     sed -i "s/REPLACE_ME_WITH_CLIENT_NUM/$i/g" $clientFile
     sed -i "s/REPLACE_ME_WITH_NAMESPACE/$TNA_NAMESPACE/g" $clientFile
     echo "==== Created client file: $clientFile"
-    cat $clientFile
-    echo ""
 done
 
 ################ Set up namespace #######################
@@ -100,8 +111,9 @@ kubectl create namespace $TNA_NAMESPACE
 echo "Done!"
 
 ############### Start Server(s) #########################
+echo "==== Starting servers..."
 for ((i=1; i<=$npairs; i++)); do
-    kubectl apply -f $ymlDir/server$i.yml
+    kubectl apply -f $outdir/server$i.yml
 done
 
 echo "==== Waiting for server pods to have status of 'Running': "
@@ -119,17 +131,28 @@ echo "==== Server pods running! Waiting an extra 5 seconds before starting clien
 sleep 5
 
 ############### Start Clients ############################
+echo "==== Starting clients..."
 for ((i=1; i<=$npairs; i++)); do
     # Get server ip (we had to wait for the server to start running to get this)
     server_ip=$(kubectl get pod server-$i -n $TNA_NAMESPACE --template '{{.status.podIP}}')
-    echo "==== server-$i ip is: $server_ip"
-    sed -i "s/REPLACE_ME_WITH_SERVER_IP/$server_ip/g" $ymlDir/client$i.yml
-    cat $ymlDir/client$i.yml
-    kubectl apply -f $ymlDir/client$i.yml
+    sed -i "s/REPLACE_ME_WITH_SERVER_IP/$server_ip/g" $outdir/client$i.yml
+    kubectl apply -f $outdir/client$i.yml
+done
+
+echo "=== Waiting for netperf to run"
+for ((i = 1; i<=$npairs; i++)); do
+    clientdone=0
+    while [ "$clientdone" -ne 1 ]
+    do
+        if kubectl logs -n $TNA_NAMESPACE client-$i | grep "NETPERF_DONE"; then
+            clientdone=1
+            echo "==== Client $i is done! Logging to $outdir/client-$i.log!"
+            kubectl logs -n $TNA_NAMESPACE client-$i > $outdir/client-$i.log
+        fi
+    sleep 1
+    done
 done
 
 ############### Cleanup #################################
-echo "==== Cleanup: deleting directory and contents of $ymlDir"
-rm -rf $ymlDir
 echo "==== Deleting tna-test namespace"
-#kubectl delete namespace $TNA_NAMESPACE
+kubectl delete namespace $TNA_NAMESPACE
