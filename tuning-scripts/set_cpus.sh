a8ed0e1ec59383d7bcf9c9174a583307b65f5133
Erika Hunhoff <hunhoff.erika@gmail.com>
2024-01-09 11:57:57 -0700

    Update set cpus tuning script

--- tuning-scripts/set_cpus.sh
+++ tuning-scripts/set_cpus.sh
@@ -4,8 +4,7 @@ count=$1 #number of cpus
 begin=1
 i=0
 
-if1_name=ens1f1np1
-if2_name=ens1f0np0
+IFNAME=myiface
 
 echo "Killing irqbalance"
 killall irqbalance
@@ -17,16 +16,15 @@ echo "Disabling CPUs"
 ./disable_cpus_no_ht
 
 echo "Enabling CPUs"
-while [[ $i -lt $count ]]
+while [[ $((i + begin)) -lt $count ]]
 do
     #sudo ip route del 10.10.$((i + begin)).0/24 via 10.10.2.1
     echo 1 > /sys/devices/system/cpu/cpu$((i + begin))/online
-	((i = i + 1))
+    ((i = i + 1))
 done
 
 echo "Configuring HW queues"
-ethtool -L ens1f1np1 combined $count
-ethtool -L ens1f0np0 combined $count
+ethtool -L $IFNAME combined $count
 
 echo "Setting IRQ affinity"
 i=1
@@ -37,5 +35,4 @@ do
 	((i = i + 1)) 
 done
 
-./set_irq_affinity_cpulist.sh $cpu_list ens1f0np0
-./set_irq_affinity_cpulist.sh $cpu_list ens1f1np1
+./set_irq_affinity_cpulist.sh $cpu_list $IFNAME
